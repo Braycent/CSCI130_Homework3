45d8580ad8548771f44d5d4498ace3276323b578
Braycent <111770283+Braycent@users.noreply.github.com>
2022-09-27 15:49:41 +0000

    No change: Text is established for user to understand formatting when calculating slope between two points

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//*************************************General Math*************************************//
+// Write a C++ program to calculate and display the value of the slope of the line connecting two points with the coordinates (3,7) and (8,12). Use the fact that the slope between two points with the coordinates (x1,y1) and (x2,y2) is (y2 - y1) / (x2 - x1).//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  cout << "The formula for calulating slope is \n     (y2 - y1)/(x2 - x1)" << endl;
+  cout << "Note:\n The coordinates (3,7), (8,12) are formatted (x1,y1), (x2,y2) or (x2,y2), (x1,y1)" << endl;
+  
+}
